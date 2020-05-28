ce9d7fa13c92f2d7f8d52e5f76d1b159da793026
GorobetsAndrii <andrey.gorobets99@gmail.com>
2020-05-28 02:40:33 +0200

    Added Solver with method for search column with minimum quantity of elements

--- src/main/java/app/model/Solver.java
+++ src/main/java/app/model/Solver.java
@@ -2,10 +2,32 @@ package app.model;
 
 public class Solver {
     private DLLMatrix matrix;
+    private int[][] arr;
 
     public Solver(int[][] tab) throws Exception {
         matrix = new DLLMatrix(tab);
+        arr = tab;
     }
 
+
+    private DLLHeader getHeaderWithMinQuantityElem() {
+        DLLHeader tmp = matrix.getHead();
+        DLLHeader result = tmp;
+        int min = Integer.MAX_VALUE;
+        int col = 0;
+        while (tmp != null) {
+            if (tmp.getNumberOfElements() < min) {
+                min = tmp.getNumberOfElements();
+                col = tmp.getColumn();
+            }
+            tmp = (DLLHeader) tmp.getRight();
+        }
+
+        while (col > 0) {
+            result = (DLLHeader) result.getRight();
+            col--;
+        }
+        return result;
+    }
 
 }
